455063a53440c210f9c04b2259448f75cda47115
kunal260100 <everybodyisfantastic8@gmail.com>
2024-01-09 22:01:31 +0530

    update

---
+++ gui.py
@@ -0,0 +1,75 @@
+import tkinter as tk 
+from tkinter import filedialog
+from tkinter import *
+import tensorflow as tf
+# from model_eval_program import model
+import matplotlib.pyplot as plt
+from keras.models import load_model
+from PIL import ImageTk, Image
+import numpy as np 
+import cv2
+
+model = load_model(r'/home/kunal/CNN/model.h5')
+
+def mouth_detection():
+    
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    return model
+
+top = tk.Tk()
+top.geometry('640x480')
+top.title('Mouth Detector')
+
+label1 = Label(top, background='#CDCDCD', font=('arial',15,'bold'))
+sign_image = Label(top)
+
+def detect(file_path):
+    global Label_packed
+
+    try:
+        image = cv2.imread(file_path)
+        resize = tf.image.resize(image, (256, 256))
+        plt.imshow(resize.numpy().astype(int))
+        yhat = model.predict(np.expand_dims(resize/255, 0))
+        print(yhat)
+        label1.configure(foreground='#011638', text= 'Open' if yhat > 0.5 else 'Close')
+
+        if yhat > 0.5:
+            print(f'Mouth is open')
+        else:
+            print(f'Mouth is closed')
+
+    except:
+        label1.configure(foreground='#011638', text='Unable to detect')
+
+def show_detect_button(file_path):
+    detect_b = Button(top, text='Detect Mouth', command=lambda : detect(file_path),pady=5, padx=10)
+    detect_b.configure(background='#011638', foreground='white', font=('arial', 10, 'bold'))
+    detect_b.place(relx=0.79, rely=0.46)
+
+
+def upload_image():
+    try:
+        file_path = filedialog.askopenfilename()
+        uploaded = Image.open(file_path)
+        uploaded.thumbnail(((top.winfo_width()/2.3), (top.winfo_height()/2.3)))
+        im = ImageTk.PhotoImage(uploaded)
+
+        sign_image.configure(image=im)
+        sign_image.image=im
+        label1.configure(text='')
+        show_detect_button(file_path)
+    except:
+        pass
+
+upload = Button(top,text='Upload Image', command=upload_image,padx=10, pady=5)
+upload.configure(background='#364156', foreground='white', font=('arial', 10, 'bold'))
+upload.pack(side='bottom', pady=50)
+sign_image.pack(side='bottom', expand='True')
+label1.pack(side='bottom', expand='True')
+heading = Label(top, text='Mouth Detection', pady=20, font=('arial', 10, 'bold'))
+heading.configure(background='#CDCDCD', foreground='#364156')
+heading.pack()
+top.protocol("WM_DELETE_WINDOW", top.quit)
+top.mainloop()
---
+++ model_eval_program.py
@@ -0,0 +1,74 @@
+##### Importing libraries
+
+import tensorflow as tf 
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import keras
+from keras.metrics import Precision, Recall, BinaryAccuracy
+from keras.models import load_model
+
+
+##### Importing model
+
+model = load_model(r'/home/kunal/CNN/model.h5')
+
+
+###### Loading data
+
+data = '/home/kunal/CNN/mouth'
+
+parse = keras.utils.image_dataset_from_directory(data)
+
+###### Scaling data
+
+parse = parse.map(lambda x,y : (x/255, y))
+parsed_iterator =  parse.as_numpy_iterator()
+batch = parsed_iterator.next()
+
+print(batch)
+
+fig, ax = plt.subplots(ncols=4, figsize=(12,12))
+for index, img in enumerate(batch[0][:4]):
+    ax[index].imshow(img)
+    ax[index].title.set_text(batch[1][index])
+
+print(ax[index].imshow(img))
+print(ax[index].title.set_text(batch[1][index]))
+
+print(len(parse))
+
+
+train_size = int(len(parse)*0.7)
+val_size = int(len(parse)*0.2)
+test_size = int(len(parse)*0.1)
+
+print(train_size, val_size, test_size)
+
+print(train_size+val_size+test_size)
+
+###### Blending data into train, val and test
+
+train = parse.take(train_size)
+val = parse.skip(train_size).take(val_size)
+test = parse.skip(train_size+val_size).take(test_size)
+
+
+print(train, val, test)
+
+
+##### Evaluating model
+
+pre = Precision()
+re = Recall()
+acc = BinaryAccuracy()
+
+for batch in test.as_numpy_iterator():
+    X, y = batch
+    yhat = model.predict(X)
+    pre.update_state(y, yhat)
+    re.update_state(y, yhat)
+    acc.update_state(y, yhat)
+
+print(pre.result(), re.result(), acc.result())
